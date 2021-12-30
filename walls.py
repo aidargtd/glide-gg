02931160dd63d15db2ff384c0f1e06d493729f0f
sk3feel <kraminkarim@mail.ru>
2021-12-30 03:30:59 +0300

    update

--- walls.py
+++ walls.py
@@ -13,29 +13,29 @@ pygame.init()
 # 3 - LF_DOWN - постоянные движения по горизонтали
 # 4 - TWIST - кручение платформы
 class LfDownObstacle(pygame.sprite.Sprite):
-    def __init__(self, *obst_params):
+    def __init__(self, *args):
         super().__init__(all_sprites)
         self.x_move, self.y_move = BASE_X_MOVE, BASE_Y_MOVE
-        self.image = load_image(obst_params[INX_IMG_NAME])
+        self.image = load_image(args[INX_IMG_NAME])
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x, self.rect.y, self.angle = \
-            obst_params[INX_X_POS], obst_params[INX_Y_POS], obst_params[INX_STATIC_ANGLE]
-        self.speed_x, self.speed_y = obst_params[INX_X_SPEED], obst_params[INX_Y_SPEED]
+            args[INX_X_POS], args[INX_Y_POS], args[INX_STATIC_ANGLE]
+        self.speed_x, self.speed_y = args[INX_X_SPEED], args[INX_Y_SPEED]
         self.y_start_down, self.y_end_down, self.step_speed_down = [UNDEFINED] * 3
         self.left_board_lf, self.right_board_lf, self.step_speed_lf, self.frames = [UNDEFINED] * 4
-        self.load_down_move_args()
-        self.load_lf_move_args()
+        self.load_down_move_args(args)
+        self.load_lf_move_args(args)
 
-    def load_down_move_args(self):
-        self.y_start_down = obst_params[INX_Y_START_DOWN]
-        self.y_end_down = obst_params[INX_Y_END_DOWN]
-        self.step_speed_down = obst_params[INX_STEP_SPEED_DOWN]
+    def load_down_move_args(self, move_inf):
+        self.y_start_down = move_inf[INX_Y_START_DOWN]
+        self.y_end_down = move_inf[INX_Y_END_DOWN]
+        self.step_speed_down = move_inf[INX_STEP_SPEED_DOWN]
 
-    def load_lf_move_args(self):
-        self.left_board_lf = obst_params[INX_LEFT_BOARD_LF]
-        self.right_board_lf = obst_params[INX_RIGHT_BOARD_LF]
-        self.step_speed_lf = obst_params[INX_STEP_SPEED_LF]
+    def load_lf_move_args(self, move_inf):
+        self.left_board_lf = move_inf[INX_LEFT_BOARD_LF]
+        self.right_board_lf = move_inf[INX_RIGHT_BOARD_LF]
+        self.step_speed_lf = move_inf[INX_STEP_SPEED_LF]
         self.frames = ZERO_FRAMES
 
     def update(self):
@@ -63,22 +63,23 @@ class LfDownObstacle(pygame.sprite.Sprite):
 
 
 class SlideSideObstacle(pygame.sprite.Sprite):
-    def __init__(self, *obst_params):
+    def __init__(self, *args):
         super().__init__(all_sprites)
         self.x_move, self.y_move = BASE_X_MOVE, BASE_Y_MOVE
-        self.image = load_image(obst_params[INX_IMG_NAME])
+        self.image = load_image(args[INX_IMG_NAME])
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x, self.rect.y, self.angle = \
-            obst_params[INX_X_POS], obst_params[INX_Y_POS], obst_params[INX_ANGLE]
-        self.speed_x, self.speed_y = obst_params[INX_X_SPEED], obst_params[INX_Y_SPEED]
+            args[INX_X_POS], args[INX_Y_POS], args[INX_ANGLE]
+        self.speed_x, self.speed_y = args[INX_X_SPEED], args[INX_Y_SPEED]
         self.y_start_side, self.y_end_side, self.step_speed_side = [UNDEFINED] * 3
-        self.load_side_move_args()
+        self.load_side_move_args(args)
 
-    def load_side_move_args(self):
-        self.y_start_side = obst_params[INX_Y_START_SIDE]
-        self.y_end_side = obst_params[INX_Y_END_SIDE]
-        self.step_speed_side = obst_params[INX_STEP_SPEED_SIDE]
+    def load_side_move_args(self, args
+        self.y_start_side = move_inf[INX_Y_START_SIDE]
+
+    self.y_end_side = move_inf[INX_Y_END_SIDE]
+    self.step_speed_side = move_inf[INX_STEP_SPEED_SIDE]
 
     def update(self):
         if self.y_start_side <= self.rect.y <= self.y_end_side:
@@ -92,15 +93,15 @@ class SlideSideObstacle(pygame.sprite.Sprite):
 
 
 class TwistObstacle(pygame.sprite.Sprite):
-    def __init__(self, *obst_params):
+    def __init__(self, *args):
         super().__init__(all_sprites)
-        self.image = load_image(obst_params[INX_IMG_NAME])
+        self.image = load_image(args[INX_IMG_NAME])
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = obst_params[INX_X_POS], obst_params[INX_Y_POS]
-        self.speed_x, self.speed_y = obst_params[INX_X_SPEED], obst_params[INX_Y_SPEED]
+        self.rect.x, self.rect.y = args[INX_X_POS], args[INX_Y_POS]
+        self.speed_x, self.speed_y = args[INX_X_SPEED], args[INX_Y_SPEED]
         self.rotated_surface, self.rotated_rect = UNDEFINED, UNDEFINED
         self.step_angle, self.start_angle = \
-            obst_params[INX_STEP_ANG_TWIST], obst_params[INX_BASE_ANG_TWIST]
+            args[INX_STEP_ANG_TWIST], args[INX_BASE_ANG_TWIST]
 
     def update(self):
         if self.start_angle > 0:
@@ -109,7 +110,7 @@ class TwistObstacle(pygame.sprite.Sprite):
             self.start_angle = 0
             self.angle += self.infin_twist_angle
         self.rect = self.rect.move(self.speed_x, self.speed_y)
-        self.rotated_surface = pygame.transform.rotate(self.image, self.angle)
+        self.rotated_suargsame.transform.rotate(self.image, self.angle)
         self.rotated_rect = self.rotated_surface.get_rect(center=(self.rect.x, self.rect.y))
 
     def get_surf_rect(self):
