f3b7fdc9b367e1a5bd6d1d6e874b8402ac215d96
sk3feel <kraminkarim@mail.ru>
2022-01-01 15:33:56 +0300

    added new functions

--- general_functions.py
+++ general_functions.py
@@ -8,6 +8,8 @@ from parametres import *
 
 fps_clock = pygame.time.Clock()
 
+pygame.init()
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('pictures', name)
@@ -35,23 +37,20 @@ def print_text(screen, message, x, y, font_size=30, font_color=WHITE_COLOR, font
     return text.get_rect()
 
 
-def get_pict_name_by_id(id):
-    return select_one_with_aspect(IMAGES, ID, id, NAME)[0]
-
-
 def correct_obst_arr(arr):
     for i in range(len(arr)):
         arr[i] = list(arr[i][1:])
         image_id = arr[i][INX_IMG_NAME]
-        arr[i][INX_IMG_NAME] = get_pict_name_by_id(image_id)
+        img_name = select_one_with_aspect(IMAGES, ID, image_id, NAME)[0]
+        arr[i][INX_IMG_NAME] = img_name
     return arr
 
 
 def get_obstacles(id_level, obst_type):
     obstacles = select_all_with_aspect(obst_type,
                                        ID_LEVEL, id_level, *TABLES[obst_type])
-    res = []
-    for i in correct_obst_arr(obstacles):
-        res.append([HIDDEN_OBSTACLE, i])
-    return res
+    return correct_obst_arr(obstacles)
 
+
+def check_sane_y_cord(y):
+    return -200 <= y <= 1000
